79e8074dcccb31561090074baa4640c1ac104d6c
Ilyes Kharroubi <ilyes.kharroubi@gmail.com>
2025-04-29 08:38:08 +0100

    instruct.js

--- instruct.js
+++ instruct.js
@@ -1,12 +1,11 @@
-let t = 60;
+let t = 10;
 let compteur = null;
 
 function start() {
   compteur = setInterval(() => {
     if (t <= 0) {
       clearInterval(compteur);
-      move();
-      alert("Time out!")
+      move1();
     } else {
       t--;
       document.getElementById("b1").textContent = t;
@@ -19,5 +18,23 @@ function start() {
 function move() {
   window.location.href = "scrabble.html";
 }
-
+function move1(){
+  window.location.href = "stat.html"
+}
+function move2(){
+  window.location.href = "Authentification.html"
+}
+function affich(){
+  let a = document.getElementById("b5")
+  a.innerHTML = "Time out!";
+  a.style.display = "block";
+  a.style.opacity =1;
+}
 start();
+function valider(){
+  let mail =document.forms["forms"]["mail"].value;
+  let username =document.forms["forms"]["username"].value;
+  let pass =document.forms["forms"]["password"].value;
+
+  return true;
+}
